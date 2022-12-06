460e617fa1a2b695ad3a343b64437e03c4f44bcb
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-06 16:42:21 +0300

    Served static images and added error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -10,6 +12,7 @@ const feedRoutes = require("./routes/feed");
 dotenv.config();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,6 +26,13 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 mongoose
   .connect(process.env.MONGO_DB)
   .then((result) => {
@@ -31,4 +41,3 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
-
