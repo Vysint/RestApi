8ad8f4feb5812e03435fe0052e219c115b4e21f9
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-07 13:13:04 +0300

    Added user signup Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const { body } = require("express-validator");
+const user = require("../models/user");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return user.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.Signup
+);
 
 module.exports = router;
